ea0dc5f271a1f82db449a1af842deadb124f0771
JeremyKungu <jeremikungu27@gmail.com>
2023-02-01 11:52:44 +0300

    added login functionality

--- to-your-door/src/components/Login.jsx
+++ to-your-door/src/components/Login.jsx
@@ -4,11 +4,44 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
 function Login({ onLogin, setUser}) {
-    // const [username, setUsername] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [errors, setErrors] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState("");
 
-    // const navigate = useNavigate;
+    const navigate = useNavigate;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = {
+            username: username,
+            password: password,
+        };
+
+        fetch("http://localhost:3000/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then((response) => {
+            if (response.status > 199 && response.status < 300) {
+                response.json().then((data) => {
+                    console.log(data)
+                    localStorage.setItem("user", JSON.stringify(data.customer));
+                    localStorage.setItem("token", data.jwt);
+                    navigate("/")
+                    setUser(formData.username)
+                });
+            } else {
+                response.json().then((err) => {
+                    console.log(err)
+                    setErrors(err.error);
+                })
+            }
+        })
+    };
 
   return (
     <div className='login-container'>
@@ -25,6 +58,7 @@ function Login({ onLogin, setUser}) {
                      type="text"
                      placeholder='Username'
                      name='username'
+                     onChange={(e) => setUsername(e.target.value)}
                     ></input>
                 </span>
                 <br></br>
@@ -35,11 +69,12 @@ function Login({ onLogin, setUser}) {
                      type="password"
                      placeholder='password'
                      name='password'
+                     onChange={(e) => setPassword(e.target.value)}
                     ></input>
                 </span>
                 <br></br>
 
-                <button type='submit' id='login-btn'>
+                <button type='submit' onClick={(e) => handleSubmit(e)} id='login-btn'>
                     Login
                 </button>
 
@@ -49,7 +84,6 @@ function Login({ onLogin, setUser}) {
                 </div>
             </form>
         </div>
-
     </div>
   )
 }
