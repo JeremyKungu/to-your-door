0e981ea58c03c4916b34347ca52d2ab4245dd25f
JeremyKungu <jeremikungu27@gmail.com>
2023-02-02 11:48:20 +0300

    added singleproduct component

---
+++ to-your-door/src/components/SingleProduct.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./SingleProduct.css";
+
+function SingleProduct({ foodData }) {
+    // const {
+    //     state: { cart },
+    //     dispatch,
+    // } = CartState();
+    console.log(foodData)
+  return (
+    <div className='single-food-card'>
+        {foodData.map((food) => (
+            <div className='foodCard'>
+                <img src={food.img_url} alt="food-item" />
+                <p className='food_name'>{food.name}</p>
+                <p>Price: {food.price}</p>
+                <p>{food.category}</p>
+                <Form>
+                    <Form.Group className='quantity-box'>
+                        <div className='order-action'>
+                            {cart.some((p) => p.id === food.id) ? (
+                                <Button
+                                onClick={() => {
+                                    dispatch({
+                                        type: "REMOVE_FROM_CART",
+                                        payload: food,
+                                    });
+                                }}
+                                variant="danger"
+                                >Remove from Cart</Button>
+                            ) : (
+                                <Button
+                                onClick={() => {
+                                    dispatch({
+                                        type: "ADD_TO_CART",
+                                        payload: food,
+                                    });
+                                }}
+                                >Add to Cart</Button>
+                            )}
+                        </div>
+                    </Form.Group>
+                </Form>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default SingleProduct
